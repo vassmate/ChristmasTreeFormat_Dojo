46c4803228130a579ad88f87fc665ed6b8b4edb3
vassmate <vassmate08@gmail.com>
2016-10-06 11:20:06 +0200

    Simplified the code

--- src/christmastree_dojo/ChristmasTreeFormat.java
+++ src/christmastree_dojo/ChristmasTreeFormat.java
@@ -7,37 +7,25 @@ public class ChristmasTreeFormat {
 		if (input == null || input.equals("")) {
 			return result;
 		}
-
-		String currentChar = "";
-		int charRowCount = 3;
 		int spaceCount = input.length() - 1;
+		int rowCharCount = input.length();
 
 		for (int i = 0; i < input.length(); i++) {
-			currentChar = String.valueOf(input.charAt(i));
-			if (i == 0) {
-				result.append(makeRow(spaceCount, 1, currentChar));
-				spaceCount--;
-			} else {
-				result.append("\n" + makeRow(spaceCount, charRowCount, currentChar));
-				spaceCount--;
-				charRowCount += 2;
+			for (int c = 0; c < rowCharCount; c++) {
+				if (c < spaceCount) {
+					result.append(" ");
+				} else {
+					result.append(String.valueOf(input.charAt(i)));
+				}
 			}
+			rowCharCount++;
+			spaceCount--;
+			result.append("\n");
 		}
 		return result;
 	}
 
-	private String makeRow(int spaceCnt, int charRowCnt, String charToAppend) {
-		String row = "";
-		for (int p = 0; p < spaceCnt; p++) {
-			row += " ";
-		}
-		for (int j = 0; j < charRowCnt; j++) {
-			row += charToAppend;
-		}
-		return row;
-	}
-
 	public static void main(String[] args) {
-		System.out.println(new ChristmasTreeFormat().formatter("hello").toString());
+		System.out.println(new ChristmasTreeFormat().formatter("bye").toString());
 	}
 }
